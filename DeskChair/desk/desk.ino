742cf6ad374993aac7058d1b50565ee379c1b793
groger63 <quentin.chambefort@gmail.com>
2017-03-12 11:09:14 +0100

    alors là ça marche

--- DeskChair/desk/desk.ino
+++ DeskChair/desk/desk.ino
@@ -14,17 +14,17 @@
 
 //-------------------Motors-------------------
 
-const int MOTOR1_BACK = 22;
-const int MOTOR1_FORWARD = 24;
+const int DOSSIER_BACK = 24;
+const int DOSSIER_FORWARD = 22;
 
-const int MOTOR2_BACK = 26;
-const int MOTOR2_FORWARD = 28;
+const int HAUTEUR_BACK = 28;
+const int HAUTEUR_FORWARD = 26;
 
-const int MOTOR3_BACK = 30;
-const int MOTOR3_FORWARD = 32;
+const int AVANCEMENT_BACK = 32;
+const int AVANCEMENT_FORWARD = 30;
 
-const int MOTOR4_BACK = 34;
-const int MOTOR4_FORWARD = 36;
+const int ASSISE_BACK = 36;
+const int ASSISE_FORWARD = 34;
 
 
 //-------------------Potentiometers-------------------
@@ -49,25 +49,18 @@ const int POTENTIOMETER4 = A4;    // select the input pin for the potentiometer
 
 
 
-const int BUTTON_DOSSIER_FORWARD = 23;     // the number of the pushbutton pin
-const int BUTTON_DOSSIER_BACK = 25;     // the number of the pushbutton pin
+const int BUTTON_HAUTEUR_FORWARD = 33;//23     // the number of the pushbutton pin
+const int BUTTON_HAUTEUR_BACK = 31; //25    // the number of the pushbutton pin
 
-const int BUTTON_ASSISE_FORWARD = 0;     // the number of the pushbutton pin
-const int BUTTON_ASSISE_BACK = 1;     // the number of the pushbutton pin
+const int BUTTON_ASSISE_FORWARD = 23;//31     // the number of the pushbutton pin
+const int BUTTON_ASSISE_BACK = 25;   //33  // the number of the pushbutton pin
 
-const int BUTTON_AVANCEMENT_FORWARD = 2;     // the number of the pushbutton pin
-const int BUTTON_AVANCEMENT_BACK = 3;     // the number of the pushbutton pin
+const int BUTTON_DOSSIER_FORWARD = 35;     // the number of the pushbutton pin
+const int BUTTON_DOSSIER_BACK = 37;     // the number of the pushbutton pin
 
-const int BUTTON_HAUTEUR_FORWARD = 5;     // the number of the pushbutton pin
-const int BUTTON_HAUTEUR_BACK = 6 ;    // the number of the pushbutton pin
-//const int BUTTON_ASSISE_FORWARD = 27;     // the number of the pushbutton pin
-//const int BUTTON_ASSISE_BACK = 29;     // the number of the pushbutton pin
 
-//const int BUTTON_AVANCEMENT_FORWARD = 31;     // the number of the pushbutton pin
-//const int BUTTON_AVANCEMENT_BACK = 33;     // the number of the pushbutton pin
-
-//const int BUTTON_HAUTEUR_FORWARD = 35;     // the number of the pushbutton pin
-//const int BUTTON_HAUTEUR_BACK = 37 ;    // the number of the pushbutton pin
+const int BUTTON_AVANCEMENT_FORWARD = 27;     // the number of the pushbutton pin
+const int BUTTON_AVANCEMENT_BACK = 29 ;    // the number of the pushbutton pin
 
 
 //-------------------Misc-------------------
@@ -88,10 +81,13 @@ int memory_state4 = 512;
 
 //-------------------Motor units-------------------
 
-Motor assise(MOTOR1_BACK,MOTOR1_FORWARD,POTENTIOMETER1,MARGIN_MOTOR) ;
-Motor avancement(MOTOR2_BACK,MOTOR2_FORWARD,POTENTIOMETER2,MARGIN_MOTOR) ;
-Motor hauteur(MOTOR3_BACK,MOTOR3_FORWARD,POTENTIOMETER3,MARGIN_MOTOR) ;
-Motor dossier(MOTOR4_BACK,MOTOR4_FORWARD,POTENTIOMETER4,MARGIN_MOTOR) ;
+Motor assise(ASSISE_BACK,ASSISE_FORWARD,POTENTIOMETER1,MARGIN_MOTOR) ;
+
+Motor avancement(AVANCEMENT_BACK,AVANCEMENT_FORWARD,POTENTIOMETER2,MARGIN_MOTOR) ;
+
+Motor hauteur(HAUTEUR_BACK,HAUTEUR_FORWARD,POTENTIOMETER3,MARGIN_MOTOR) ;
+
+Motor dossier(DOSSIER_BACK,DOSSIER_FORWARD,POTENTIOMETER4,MARGIN_MOTOR) ;
 
 
 //-------------------Seat-------------------
@@ -143,7 +139,8 @@ void loop() {
        seat.motor_dossier.go_backward();
      
      
-   } 
+   }
+  else seat.motor_dossier.go_stop();
    
    if (digitalRead(BUTTON_ASSISE_FORWARD) == HIGH) {
        
@@ -156,7 +153,9 @@ void loop() {
        seat.motor_assise.go_backward();
           
      
-   } 
+   }else  seat.motor_assise.go_stop();
+   
+   
    if (digitalRead(BUTTON_AVANCEMENT_FORWARD) == HIGH) {
        
        seat.motor_avancement.go_forward();
@@ -168,7 +167,8 @@ void loop() {
        seat.motor_avancement.go_backward();
      
      
-   }
+   }else seat.motor_avancement.go_stop();
+   
    
      if (digitalRead(BUTTON_HAUTEUR_FORWARD) == HIGH) {
        
@@ -180,23 +180,9 @@ void loop() {
        
        seat.motor_hauteur.go_backward();
        
-   }
+   }else seat.motor_hauteur.go_stop();
    
-   if(  digitalRead(BUTTON_DOSSIER_FORWARD) == LOW
-     && digitalRead(BUTTON_DOSSIER_BACK) == LOW 
-     && digitalRead(BUTTON_ASSISE_FORWARD) == LOW 
-     && digitalRead(BUTTON_ASSISE_BACK) == LOW 
-     && digitalRead(BUTTON_AVANCEMENT_FORWARD) == LOW 
-     && digitalRead(BUTTON_AVANCEMENT_BACK) == LOW 
-     && digitalRead(BUTTON_HAUTEUR_FORWARD) == LOW 
-     && digitalRead(BUTTON_HAUTEUR_BACK) == LOW )
-     {
-       seat.motor_hauteur.go_stop();
-       seat.motor_dossier.go_stop();
-       seat.motor_assise.go_stop();
-       seat.motor_avancement.go_stop();
-
-     }
+   
   
   
 }
